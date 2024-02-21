1e584af26e9a93eb7129785a3c307b7dbd8bc4fa
Daran Dachte <darandachte@gmail.com>
2024-02-21 22:13:25 +0100

    section5-6 created

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/image", "@vueuse/nuxt"],
+
   devtools: { enabled: true },
 });
