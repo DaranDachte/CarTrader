2e33c26be1565ab4e99184ebc8b8db75b21b45d1
Daran Dachte <darandachte@gmail.com>
2024-02-26 14:08:26 +0100

    bg fixed

--- components/Car/Hero.vue
+++ components/Car/Hero.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="relative h-screen w-100 bg-no-repeat bg-cover bg-bottom overflow-auto bg-[url('D:\VueWithUdemi\CarTrader\img\carBg.jpg')]"
+    class="relative h-screen w-100 bg-no-repeat bg-cover bg-bottom overflow-auto backgroundImage"
   >
     <div class="z-10 absolute w-full h-full flex justify-center items-center">
       <div class="text-center w-1/2">
@@ -11,3 +11,9 @@
     <div class="z-1 absolute w-full h-full bg-black opacity-50"></div>
   </div>
 </template>
+
+<style scoped>
+.backgroundImage {
+  background-image: url("../../img/carBg.jpg");
+}
+</style>
